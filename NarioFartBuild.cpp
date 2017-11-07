c88112eb6b9fffb6e0c85fdd787b90609e094cdf
TheMOK <ilyamokka@gmail.com>
2017-11-07 17:45:53 +0300

    PEEBUILD

--- NarioFartBuild.cpp
+++ NarioFartBuild.cpp
@@ -9,14 +9,23 @@ namespace graphicsWorker {
     }
 
     void drawRectangle(int left, int up, int right, int down, int color) {
-        txSetFillColor(color);
+        switch(color) {
+            case 0:
+                txSetFillColor(RGB(100, 100, 255));
+                txSetColor(RGB(100, 100, 255));
+                break;
+            case 1:
+                txSetFillColor(RGB(0, 200, 0));
+                txSetColor(RGB(0, 200, 0));
+                break;
+        }
         txRectangle(left, up, right, down);
     }
 }
 
 class Cube {
     public:
-    virtual void draw(int cubeSide, int x, int y) = 0;
+    virtual void draw(int cubeSide, float x, int y) = 0;
 };
 
 class AirCube : public Cube {
@@ -26,8 +35,8 @@ class AirCube : public Cube {
     AirCube& operator=(Cube&);
 
     public:
-    void draw(int cubeSide, int x, int y) {
-        graphicsWorker::drawRectangle(cubeSide * x, cubeSide * y, cubeSide * (x + 1), cubeSide * (y + 1), 100);
+    void draw(int cubeSide, float x, int y) {
+        graphicsWorker::drawRectangle(cubeSide * x, cubeSide * y, cubeSide * (x + 1), cubeSide * (y + 1), 0);
     }
 
     static AirCube* getInstance() {
@@ -43,7 +52,9 @@ class DirtCube : public Cube {
     DirtCube& operator=(Cube&);
 
     public:
-    void draw(int cubeSide, int x, int y) {}
+    void draw(int cubeSide, float x, int y) {
+        graphicsWorker::drawRectangle(cubeSide * x, cubeSide * y, cubeSide * (x + 1), cubeSide * (y + 1), 1);
+    }
 
     static DirtCube* getInstance() {
         static DirtCube instance;
@@ -73,7 +84,9 @@ class Map {
     private:
 //    static const int _fragmentsNumber = 3;
 //    MapFragment _fragments[_fragmentsNumber];
-    static int const mapWidth = 15, mapHeight = 10;
+    public:
+    static int const mapWidth = 200, mapHeight = 12;
+    private:
     int cubeSide;
     Cube *_cubes[mapWidth][mapHeight];
     public:
@@ -82,18 +95,30 @@ class Map {
         //initWithFile
         for(int x = 0; x < mapWidth; x++) {
             for(int y = 0; y < mapHeight; y++) {
-                _cubes[x][y] = AirCube::getInstance();
+                if (x < mapWidth - 1 && x > 0 && y < mapHeight - 1) _cubes[x][y] = AirCube::getInstance();
+                else _cubes[x][y] = DirtCube::getInstance();
+                if (x % 10 == 0 && y == mapHeight - 2) _cubes[x][y] = DirtCube::getInstance();
+                if (x % 15 == 0 && y == mapHeight - 3) _cubes[x][y] = DirtCube::getInstance();
+                if (x % 20 == 0 && y == mapHeight - 4) _cubes[x][y] = DirtCube::getInstance();
+                if (x % 25 == 0 && y == mapHeight - 5) _cubes[x][y] = DirtCube::getInstance();
+                if (x % 30 == 0 && y == mapHeight - 6) _cubes[x][y] = DirtCube::getInstance();
+                if (x % 35 == 0 && y == mapHeight - 7) _cubes[x][y] = DirtCube::getInstance();
             }
         }
     }
 
-    void draw(float centerX) {
-        for(int x = 0; x < mapWidth; x++) {
+    void draw(float centerX, int gameViewWidth) {
+        for(int x = (int)floor(0-centerX); x <= gameViewWidth / cubeSide + (int)floor(0-centerX); x++) {
+            if (x == mapWidth) continue;
             for(int y = 0; y < mapHeight; y++) {
                 Cube *c = _cubes[x][y];
                 c->draw(cubeSide, x + centerX, y);
             }
         }
+    }
+
+    int getCubeSide() {
+        return cubeSide;
     }
 };
 
@@ -110,7 +135,12 @@ class Game {
     }
 
     void drawCubes() {
-        _cubeMap.draw(0);
+        static float x = 1;
+        float px = x;
+        if(px > 0) px = 0;
+        if(px < 0 - Map::mapWidth + _gameViewWidth / _cubeMap.getCubeSide()) px = 0 - Map::mapWidth + _gameViewWidth / _cubeMap.getCubeSide();
+        _cubeMap.draw(px, _gameViewWidth);
+        x-=0.05;
     }
 
     void drawMobs() {}
@@ -142,6 +172,5 @@ class WinDirector {
 int main() {
     while(true) {
         WinDirector::getInstance().drawGame();
-        Sleep(2);
     }
 }
