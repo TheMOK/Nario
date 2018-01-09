7ade676d5cbb6aff80dce12e4c6f903bad7710c4
CuriousGeorgiy <32464072+CuriousGeorgiy@users.noreply.github.com>
2018-01-09 14:43:55 +0200

    added Nario class
    added Nario class, asserts, basic movement for Nario

--- NarioFartBuild.cpp
+++ NarioFartBuild.cpp
@@ -1,3 +1,6 @@
+//TODO Gen map with init file; synchronize map movement with Nario's; create frames generation for
+//TODO Nario's movement, esp jump;
+
 #include <txlib.h>
 #include <conio.h>
 
@@ -20,18 +23,19 @@ namespace graphicsWorker {
     void drawRectangle(int left, int up, int right, int down, int color) {
         switch(color) {
             case 0:
-                txSetFillColor(SKY_BLUE);
-                txSetColor    (SKY_BLUE);
+                txSetFillColor (  SKY_BLUE);
+                txSetColor     (  SKY_BLUE);
                 break;
             case 1:
                 txSetFillColor(GRASS_GREEN);
                 txSetColor    (GRASS_GREEN);
                 break;
             case 2:
-                txSetFillColor(RED);
-                txSetColor    (RED);
+                txSetFillColor(        RED);
+                txSetColor    (        RED);
                 break;
         }
+
         txRectangle(left, up, right, down);
     }
 
@@ -40,81 +44,154 @@ namespace graphicsWorker {
         return color;
     }
 
-
-
     void freezeWindow(int time) {
         txSleep(time);
     }
 }
 
-class Mario {
+class Nario {
     private:
-    static const int marioHeight =  40, marioWidth = 20, marioXSpeed = 10, marioYSpeed = 30, MARIO_COLOR = 2;
-    int marioXCenter = 65, marioYCenter = 0;
+    static const int narioHeight =  40, narioWidth = 20, narioXSpeed = 10, narioYSpeed = 20;
 
     public:
-    Mario(int cubeSide) {
-        marioYCenter = cubeSide * 11 - marioHeight / 2;
+    int narioXo = 65, narioYo = 0;
+    int narioXCenter = narioXo, narioYCenter;
+
+    Nario(int cubeSide) {
+        narioYo = cubeSide * 11 - narioHeight / 2;
+        narioYCenter = narioYo;
     }
 
-//{    a temp crutch so that mario sprite doesn't get covered by cubes and vice versa
-//    bool checkIfShouldDrawMario(int x, int y) {
-//        int flag = 1;
-//        for(x -= marioWidth / 2; x <= x + marioWidth ; x ++)
-//            for(y -= marioHeight / 2; y <= y + marioHeight; y ++)
-//                if(graphicsWorker::getPixel(x, y) == RED) flag = 0;
-//        return flag;
-//    }
-//}
-
     void draw() {
-        //if(checkIfShouldDrawMario(marioXCenter, marioYCenter))
-            graphicsWorker::drawRectangle(marioXCenter - marioWidth / 2 , marioYCenter - marioHeight / 2, marioXCenter + marioWidth / 2, marioYCenter + marioHeight / 2, 2);
+            graphicsWorker::drawRectangle(narioXCenter - narioWidth / 2 , narioYCenter - narioHeight / 2, narioXCenter + narioWidth / 2, narioYCenter + narioHeight / 2, 2);
+    }
+
+    bool checkIfOnGround(int y) {
+        if(graphicsWorker::getPixel(narioXCenter, y) == GRASS_GREEN)
+            return true;
+        else
+            return false;
+    }
+
+    bool checkIfWallLeft() {
+        int flag = 0;
+        for(int y = narioYCenter - narioHeight / 2; y <= narioYCenter + narioHeight / 2; y ++) {
+            assert(narioYCenter - narioHeight / 2 <= y && y <= narioYCenter + narioHeight / 2);
+
+            for(int x = narioXCenter - narioXSpeed - narioWidth / 2; x < narioXCenter - narioWidth / 2; x ++) {
+                assert( narioXCenter - narioXSpeed - narioWidth / 2 <= x && x < narioXCenter - narioWidth / 2);
+
+                if(graphicsWorker::getPixel(x, y) == GRASS_GREEN) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+
+    bool checkIfWallRight() {
+        int flag = 0;
+        for(int y = narioYCenter - narioHeight / 2; y <= narioYCenter + narioHeight / 2; y ++) {
+            assert(narioYCenter - narioHeight / 2 <= y && y <= narioYCenter + narioHeight / 2);
+
+            for(int x = narioXCenter + narioXSpeed + narioWidth / 2; x > narioXCenter + narioWidth / 2; x --) {
+                assert( narioXCenter + narioXSpeed + narioWidth / 2 >= x && x > narioXCenter + narioWidth / 2);
+
+                if(graphicsWorker::getPixel(x, y) == GRASS_GREEN) {
+                    flag = 1;
+                    break;
+                }
+            }
+        }
+        return flag;
     }
 
     void move() {
-        if(GetAsyncKeyState(VK_LEFT) && graphicsWorker::getPixel(marioXCenter - marioXSpeed - marioWidth / 2, marioYCenter) != GRASS_GREEN)
-            marioXCenter -= marioXSpeed;
-        else if(GetAsyncKeyState(VK_LEFT) && graphicsWorker::getPixel(marioXCenter - marioXSpeed - marioWidth / 2, marioYCenter) == GRASS_GREEN)
-        {
-            for(int x = marioXCenter - marioWidth / 2 - marioXSpeed + 1; x < marioXCenter - marioWidth / 2; x ++)
-            {
-                assert(marioXCenter - marioWidth / 2 - marioXSpeed + 1 <= x && x < marioXCenter - marioWidth / 2);
-                if(graphicsWorker::getPixel(x, marioYCenter) != GRASS_GREEN)
-                {
-                    marioXCenter = x + marioWidth / 2;
+        if(checkIfOnGround(narioYCenter + narioHeight / 2 + 1)) {
+            if(GetAsyncKeyState(VK_UP)) {
+                narioYCenter -= narioYSpeed * 3;
+                for(int y = narioYCenter; y <= narioYo + narioHeight / 2; y ++) {
+                    assert(narioYCenter <= y && y <= narioYo + narioHeight / 2);
+
+                    if(checkIfOnGround(y + narioHeight / 2)) {
+                        narioYCenter = y;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if(GetAsyncKeyState(VK_LEFT) && (!checkIfWallLeft())) {
+            if(checkIfOnGround(narioYCenter + narioHeight / 2 + 1))
+                if(GetAsyncKeyState(VK_UP))
+                    narioYCenter -= narioYSpeed * 3;
+
+            narioXCenter -= narioXSpeed;
+        }
+        else if (GetAsyncKeyState (VK_LEFT) && checkIfWallLeft()) {
+            if(checkIfOnGround(narioYCenter + narioHeight / 2 + 1))
+                if(GetAsyncKeyState(VK_UP))
+                    narioYCenter -= narioYSpeed * 3;
+
+            for (int x = narioXCenter - narioWidth / 2 - narioXSpeed + 1; x < narioXCenter - narioWidth / 2; x ++) {
+                assert (narioXCenter - narioWidth / 2 - narioXSpeed + 1 <= x && x < narioXCenter - narioWidth / 2);
+
+                if (graphicsWorker::getPixel (x, narioYCenter) != GRASS_GREEN) {
+                    narioXCenter = x + narioWidth / 2;
                     break;
                 }
             }
         }
 
-        if(GetAsyncKeyState(VK_RIGHT) && graphicsWorker::getPixel(marioXCenter + marioXSpeed + marioWidth / 2, marioYCenter) != GRASS_GREEN)
-            marioXCenter += marioXSpeed;
-        else if(GetAsyncKeyState(VK_RIGHT) && graphicsWorker::getPixel(marioXCenter + marioXSpeed + marioWidth / 2, marioYCenter) == GRASS_GREEN)
-        {
-            for(int x = marioXCenter + marioWidth / 2 + marioXSpeed - 1; x > marioXCenter + marioWidth / 2; x --)
-            {
-                assert(marioXCenter + marioWidth / 2 + marioXSpeed - 1 >= x && x > marioXCenter + marioWidth / 2);
-                if(graphicsWorker::getPixel(x, marioYCenter) != GRASS_GREEN)
-                {
-                    marioXCenter = x - marioWidth / 2;
+        if(GetAsyncKeyState(VK_RIGHT) && (!checkIfWallRight())) {
+            if(checkIfOnGround(narioYCenter + narioHeight / 2 + 1))
+                if(GetAsyncKeyState(VK_UP))
+                    narioYCenter -= narioYSpeed * 3;
+
+            narioXCenter += narioXSpeed;
+
+        }
+        else if (GetAsyncKeyState (VK_RIGHT) && checkIfWallRight()) {
+            if(checkIfOnGround(narioYCenter + narioHeight / 2 + 1))
+                if(GetAsyncKeyState(VK_UP))
+                    narioYCenter -= narioYSpeed * 3;
+
+            for (int x = narioXCenter + narioWidth / 2 + narioXSpeed - 1; x > narioXCenter + narioWidth / 2; x --) {
+                assert (narioXCenter + narioWidth / 2 + narioXSpeed - 1 >= x && x > narioXCenter + narioWidth / 2);
+
+                if (graphicsWorker::getPixel (x, narioYCenter) != GRASS_GREEN) {
+                    narioXCenter = x - narioWidth / 2;
                     break;
                 }
+            }
+        }
+
+        for(int y = narioYCenter; y <= narioYo + narioHeight / 2; y ++) {
+            assert(narioYCenter <= y && y <= narioYo + narioHeight / 2);
+
+            if(checkIfOnGround(y + narioHeight / 2)) {
+                narioYCenter = y;
+                break;
             }
         }
     }
 };
 
+// Cubes base class
 class Cube {
     public:
     virtual void draw(int cubeSide, float x, int y) = 0;
 };
 
+// Singleton class derived from Cube
 class AirCube : public Cube {
     private:
     AirCube() {}
-    AirCube(const Cube&);
-    AirCube& operator=(Cube&);
+
+    AirCube(const Cube&) = delete;
+
+    AirCube& operator=(const Cube&) = delete;
 
     public:
     void draw(int cubeSide, float x, int y) {
@@ -127,11 +204,14 @@ class AirCube : public Cube {
     }
 };
 
+// Singleton class derived from Cube
 class DirtCube : public Cube {
     private:
     DirtCube() {}
-    DirtCube(const Cube&);
-    DirtCube& operator=(Cube&);
+
+    DirtCube(const Cube&) = delete;
+
+    DirtCube& operator=(const Cube&) = delete;
 
     public:
     void draw(int cubeSide, float x, int y) {
@@ -179,11 +259,13 @@ class Map {
     public:
     Map(int gameViewHeight) {
         cubeSide = gameViewHeight / mapHeight;
-        //initWithFile
+        //initWithFile (read from file in next version)
         for(int x = 0; x < mapWidth; x++) {
             assert(0 <= x && x < mapWidth);
+
             for(int y = 0; y < mapHeight; y++) {
                 assert(0 <= y && y < mapHeight);
+
                 if (x < mapWidth - 1 && x > 0 && y < mapHeight - 1) _cubes[x][y] = AirCube::getInstance();
                 else _cubes[x][y] = DirtCube::getInstance();
                 if (x % 10 == 0 && y == mapHeight - 2) _cubes[x][y] = DirtCube::getInstance();
@@ -196,29 +278,16 @@ class Map {
         }
     }
 
-//{    a temp crutch so that mario sprite doesn't get covered by cubes and vice versa. do we need it?
-//    bool checkIfShouldDrawCube(int x, int y, int cubeSide) {
-//        int flag = 1;
-//        for(int _x = x * cubeSide; _x <= (x + 1) * cubeSide; _x ++)
-//        {
-//            for(int _y = y * cubeSide; _y <= (y + 1) * cubeSide; _y ++) {
-//                if(graphicsWorker::getPixel(_x, _y) == RED) flag = 0;
-//            }
-//        }
-//
-//        return flag;
-//    }
-//}
-
     void draw(float centerX, int gameViewWidth) {
-        for(int x = (int)floor(- centerX); x <= gameViewWidth / cubeSide + (int)floor(- centerX); x ++) {
+        for(int x = (int)floor(- centerX); x <= gameViewWidth / cubeSide + (int)floor(- centerX); x++) {
             assert((int)floor(- centerX) <= x && x <= gameViewWidth / cubeSide + (int)floor(- centerX));
+
             if (x == mapWidth) continue;
-            for(int y = 0; y < mapHeight; y ++) {
+            for(int y = 0; y < mapHeight; y++) {
                 assert(0 <= y && y < mapHeight);
+
                 Cube *c = _cubes[x][y];
-                //if(checkIfShouldDrawCube(x + centerX, y, cubeSide))  see crutch
-                    c->draw(cubeSide, x + centerX, y);
+                c->draw(cubeSide, x + centerX, y);
             }
         }
     }
@@ -232,36 +301,31 @@ class Game {
     private:
     int _gameViewWidth, _gameViewHeight, _mobsNumber;
     Map _cubeMap = Map(0);
-    Mario mario = Mario(0);
+    Nario nario = Nario(0);
 
     public:
     Game(int gameViewWidth, int gameViewHeight, int cubesVerticalNumber) {
         _cubeMap = Map(gameViewHeight);
-        mario = Mario(_cubeMap.getCubeSide());
+        nario = Nario(_cubeMap.getCubeSide());
         _gameViewWidth = gameViewWidth;
         _gameViewHeight = gameViewHeight;
-
     }
 
-    private:
-    //Mario mario = Mario(0); //crutch, dunno how to get mapViewHeight ?
-
-    public:
     void drawCubes() {
         static float x = 1;
         float px = x;
         if(px > 0) px = 0;
         if(px < - Map::mapWidth + _gameViewWidth / _cubeMap.getCubeSide()) px = - Map::mapWidth + _gameViewWidth / _cubeMap.getCubeSide();
         _cubeMap.draw(px, _gameViewWidth);
-        //x -= 0.05;
+//        x -= 0.05;
     }
 
-    void drawMario() {
-        mario.draw();
+    void drawnario() {
+        nario.draw();
     }
 
-    void moveMario() {
-        mario.move();
+    void movenario() {
+        nario.move();
     }
 
     void drawMobs() {}
@@ -274,15 +338,15 @@ class WinDirector {
     const int windowHeight = 600, windowWidth = 800;
 
     private:
-    WinDirector() {
+        WinDirector() {
             graphicsWorker::createWindow(windowWidth, windowHeight);
-    }
+        }
 
-    WinDirector(const WinDirector&);
+        WinDirector(const WinDirector&) = delete;
 
-    WinDirector& operator=(WinDirector&);
+        WinDirector& operator=(const WinDirector&) = delete;
 
-    Game game = Game(windowWidth, windowHeight, 50);
+        Game game = Game(windowWidth, windowHeight, 50);
 
     public:
     static WinDirector& getInstance() {
@@ -292,12 +356,11 @@ class WinDirector {
 
     void drawGame() {
         game.drawCubes();
-        game.drawMario();
-
+        game.drawnario();
     }
 
     void moveObjs() {
-        game.moveMario();
+        game.movenario();
     }
 };
 
